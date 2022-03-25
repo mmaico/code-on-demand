d80ceeafbf6fac60afd6433d2af380d82ff4b321
Marcelo Maico <mmaico@gmail.com>
2022-03-25 11:36:39 -0300

    Create default masks

---
+++ node/src/mask/mask.default.ts
@@ -0,0 +1,12 @@
+import {Type} from "./formatter/mask.formatter";
+
+
+export class MaskDefault {
+
+    public static masks = new Map<Type, string>([
+        [Type.CPF, "###.***.***-**"],
+        [Type.CNPJ, "**.***.***/****-##"],
+        [Type.MOBILE, "(##)*****-####"],
+        [Type.EMAIL, "##*****@#***.###.##"]
+    ])
+}
