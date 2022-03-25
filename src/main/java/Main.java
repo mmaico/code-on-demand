a43dfd263d0792ba02db446615e0683841975a72
Marcelo Maico <mmaico@gmail.com>
2022-03-25 11:39:33 -0300

    Refactory the main class to call the javascript file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,30 @@
 import jdk.nashorn.api.scripting.ScriptObjectMirror;
 
+import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
     private final static String NASHORN_ARGS = "nashorn.args";
     private final static String ES_6 = "--language=es6";
 
-    public static void main(String[] args) throws FileNotFoundException, ScriptException, NoSuchMethodException {
+    private static final String JSON = "{\"store\": { \"book\": [ { \"phone\": \"11987638765\", \"email\": \"mmaico@gmail.com\"}, { \"phone\": \"11956194823\", \"email\": \"geisa.lima@hotmail.com\"}]}}";
+    private static final String SETTINGS = "[{\"type\": \"MOBILE\", \"path\": \"$.store.book[*].phone\"}, {\"type\": \"EMAIL\", \"path\": \"$.store.book[*].email\"}]";
+
+    public static void main(String[] args) throws IOException, ScriptException, NoSuchMethodException {
         System.setProperty(NASHORN_ARGS, ES_6);
         ScriptEngineManager manager = new ScriptEngineManager();
         ScriptEngine engine = manager.getEngineByName("JavaScript");
-     //   engine.eval("load('/uol/pocs/backend-code-on-deman/node/src/jsonpath.js');");
-        //engine.eval("JSONPath.JSONPath({path: '...', json: {}})");
+        Object eval = engine.eval(Files.newBufferedReader(Paths.get("/uol/pocs/code-on-demand/src/main/resources/js-code/code-on-demand.js"), StandardCharsets.UTF_8));
 
-
-
-
-        //Object eval2 = engine.eval(new FileReader("/uol/pocs/jsonpath/generated/parser.js"));
-//        String file = "/tyityututuol/pocs/backend-code-on-deman/node/src/jvm-npm.js";
-//        engine.eval("load('"+file+"')");
-        Object eval = engine.eval(new FileReader(Main.class.getResource("js-code/code-on-demand.js").getPath()));
-
-        Object aDefault = ((ScriptObjectMirror) ((ScriptObjectMirror) eval).get("default")).call(eval);
+        Object aDefault = ((ScriptObjectMirror) ((ScriptObjectMirror) eval).get("default")).call(eval, JSON, SETTINGS);
         System.out.println(aDefault);
     }
 }
