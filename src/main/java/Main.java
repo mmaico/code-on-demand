95ba8747fb6350d9e29dab5fd8309f2dd5ff7433
Marcelo Maico <mmaico@gmail.com>
2022-03-25 15:24:28 -0300

    Change to relative path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -18,11 +19,12 @@ public class Main {
     private static final String JSON = "{\"store\": { \"book\": [ { \"phone\": \"11987638765\", \"email\": \"mmaico@gmail.com\"}, { \"phone\": \"11956194823\", \"email\": \"geisa.lima@hotmail.com\"}]}}";
     private static final String SETTINGS = "[{\"type\": \"MOBILE\", \"path\": \"$.store.book[*].phone\"}, {\"type\": \"EMAIL\", \"path\": \"$.store.book[*].email\"}]";
 
-    public static void main(String[] args) throws IOException, ScriptException, NoSuchMethodException {
+    public static void main(String[] args) throws IOException, ScriptException {
         System.setProperty(NASHORN_ARGS, ES_6);
         ScriptEngineManager manager = new ScriptEngineManager();
         ScriptEngine engine = manager.getEngineByName("JavaScript");
-        Object eval = engine.eval(Files.newBufferedReader(Paths.get("/uol/pocs/code-on-demand/src/main/resources/js-code/json-mask.js"), StandardCharsets.UTF_8));
+        String absolutePath = new File("src/main/resources/js-code/json-mask.js").getAbsolutePath();
+        Object eval = engine.eval(Files.newBufferedReader(Paths.get(absolutePath), StandardCharsets.UTF_8));
 
         Object aDefault = ((ScriptObjectMirror) ((ScriptObjectMirror) eval).get("default")).call(eval, JSON, SETTINGS);
         System.out.println(aDefault);
