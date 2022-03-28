75aca0a1efe1e0aef31ff57dd456c48472372d84
Marcelo Maico <mmaico@gmail.com>
2022-03-28 07:33:54 -0300

    Creating the repository

---
+++ src/main/java/mmaico/codeondemand/sellers/infrastructure/SellerRepositoryImpl.java
@@ -0,0 +1,35 @@
+package mmaico.codeondemand.sellers.infrastructure;
+
+import mmaico.codeondemand.sellers.domain.seller.Seller;
+import mmaico.codeondemand.sellers.domain.seller.SellerRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class SellerRepositoryImpl implements SellerRepository {
+    private  Map<String, Seller> database = new HashMap<>();
+
+    public SellerRepositoryImpl() {
+        database.put("1", new Seller("1", "Jaime Lannister", new Date(), "92170410090", "11987569852",
+                "jlannister@got.com"));
+        database.put("2", new Seller("2", "Sansa Stark", new Date(), "10782412017", "11932547896",
+                "sstark@got.com"));
+        database.put("3", new Seller("3", "Jon Snow", new Date(), "47728435078", "11921478965",
+                "sstark@got.com"));
+    }
+
+
+    @Override
+    public Optional<Seller> findOne(String id) {
+        if (database.containsKey(id)) {
+            return Optional.of(database.get(id));
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return new ArrayList<>(database.values());
+    }
+}
